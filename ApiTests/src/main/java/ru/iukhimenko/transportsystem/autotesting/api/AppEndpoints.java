36437c174e49d21ebbad611d76d87f5f3c858b9d
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-04-17 22:15:58 +0300

    Added AppEndpoints

---
+++ ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/AppEndpoints.java
@@ -0,0 +1,8 @@
+package ru.iukhimenko.transportsystem.autotesting.api;
+
+public class AppEndpoints {
+    public static final String DEFAULT_URI = "http://localhost";
+    public static String BASE_URI = DEFAULT_URI;
+    public static final String REGISTER_ENDPOINT = "/api/auth/signup";
+    public static final String USERS_ENDPOINT = "/api/auth/users";
+}
