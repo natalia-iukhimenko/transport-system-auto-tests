efd827350817a269c4c89908a806099ba0d53320
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-04-17 22:19:42 +0300

    Added User POJO

---
+++ Core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/User.java
@@ -0,0 +1,47 @@
+package ru.iukhimenko.transportsystem.autotesting.core.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class User {
+    private Integer id;
+    private String username;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @JsonIgnore
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("username")
+    public String getUsername() {
+        return username;
+    }
+
+    @JsonProperty("username")
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @JsonProperty("password")
+    public String getPassword() {
+        return password;
+    }
+
+    @JsonProperty("password")
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
