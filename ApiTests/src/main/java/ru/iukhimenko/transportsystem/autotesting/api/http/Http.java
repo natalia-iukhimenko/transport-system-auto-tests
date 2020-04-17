c32822958441c7d31f4430027823454d29eb3302
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-04-17 22:18:59 +0300

    Concatenate base url and endpoints in http methods

--- ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/http/Http.java
+++ ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/http/Http.java
@@ -7,13 +7,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Map;
+import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.BASE_URI;
 
 public class Http {
     static Logger logger = LoggerFactory.getLogger(Http.class);
 
     public static HttpResponse<JsonNode> sendGetRequest(String url, Map<String, String> headers, Map<String, Object> requestParams) {
         return Unirest
-                .get(url)
+                .get(BASE_URI + url)
                 .headers(headers)
                 .queryString(requestParams)
                 .asJson()
@@ -25,11 +26,25 @@ public class Http {
                     });
                 });
     }
-    
+
     public static HttpResponse<JsonNode> sendPostRequest(String url, Map<String, String> headers, Object body) {
         return Unirest
-                .post(url)
+                .post(BASE_URI + url)
                 .headers(headers)
+                .body(body)
+                .asJson()
+                .ifFailure(response -> {
+                    logger.error("Error Code: " + response.getStatus());
+                    response.getParsingError().ifPresent(e -> {
+                        logger.error("Parsing Exception: ", e);
+                        logger.error("Original body: " + e.getOriginalBody());
+                    });
+                });
+    }
+
+    public static HttpResponse<JsonNode> sendPostRequest(String url, Object body) {
+        return Unirest
+                .post(BASE_URI + url)
                 .body(body)
                 .asJson()
                 .ifFailure(response -> {
