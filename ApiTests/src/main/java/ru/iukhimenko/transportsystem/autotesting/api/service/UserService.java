739c1909e506ea04653f74f1dfc7fdcd308024f9
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-04-17 22:20:16 +0300

    Added UserService class and method for registration

---
+++ ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/UserService.java
@@ -0,0 +1,24 @@
+package ru.iukhimenko.transportsystem.autotesting.api.service;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.REGISTER_ENDPOINT;
+import ru.iukhimenko.transportsystem.autotesting.api.http.Http;
+import ru.iukhimenko.transportsystem.autotesting.core.model.User;
+
+public class UserService {
+    private Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    public void registerUser(User user) {
+        if (user != null) {
+            HttpResponse<JsonNode> response = Http.sendPostRequest(REGISTER_ENDPOINT, user);
+            if (response.isSuccess()) {
+                logger.info("User {0} has been registered", user.getUsername());
+            }
+        }
+        else
+            logger.warn("Registration of null user won't be performed");
+    }
+}
