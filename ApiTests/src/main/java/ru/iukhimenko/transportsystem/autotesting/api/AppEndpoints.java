a169a89a301cf381c766b1dd57d5fdf8ee27cc83
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-04-18 21:04:08 +0300

    Added authenticateUser() and getAccessToken() methods

--- ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/AppEndpoints.java
+++ ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/AppEndpoints.java
@@ -3,4 +3,5 @@ package ru.iukhimenko.transportsystem.autotesting.api;
 public class AppEndpoints {
     public static final String REGISTER_ENDPOINT = "/api/auth/signup";
     public static final String USERS_ENDPOINT = "/api/auth/users";
+    public static final String AUTH_ENDPOINT = "/api/auth/signin";
 }
--- ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/AuthService.java
+++ ApiTests/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/AuthService.java
@@ -2,8 +2,12 @@ package ru.iukhimenko.transportsystem.autotesting.api.service;
 
 import kong.unirest.HttpResponse;
 import kong.unirest.JsonNode;
+
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.AUTH_ENDPOINT;
 import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.REGISTER_ENDPOINT;
 import ru.iukhimenko.transportsystem.autotesting.api.http.Http;
 import ru.iukhimenko.transportsystem.autotesting.core.model.User;
@@ -15,10 +19,36 @@ public class AuthService {
         if (user != null) {
             HttpResponse<JsonNode> response = Http.sendPostRequest(REGISTER_ENDPOINT, user);
             if (response.isSuccess()) {
-                logger.info("User {0} has been registered", user.getUsername());
+                logger.info("User " + user.getUsername() + " has been registered");
             }
         }
         else
-            logger.warn("Registration of null user won't be performed");
+            logger.warn("Registration of null user cannot be performed");
+    }
+
+    public User authenticateUser(User user) {
+        User authenticatedUser = null;
+        if (user != null) {
+            HttpResponse<JsonNode> response = Http.sendPostRequest(AUTH_ENDPOINT, user);
+            if (response.isSuccess()) {
+                authenticatedUser = ObjectConverter.convertToObject(response.getBody().toString(), User.class);
+                logger.info("User " + user.getUsername() + " has been authenticated");
+            }
+        } else
+            logger.warn("Authentication of null user cannot be performed");
+        return authenticatedUser;
+    }
+
+    public String getAccessToken(User user) {
+        String accessToken = null;
+        HttpResponse<JsonNode> response = Http.sendPostRequest(AUTH_ENDPOINT, user);
+        if (response.isSuccess()) {
+            String type = response.getBody().getObject().getString("type");
+            String token = response.getBody().getObject().getString("token");
+            if (StringUtils.isNotEmpty(type) && StringUtils.isNotEmpty(token)) {
+                accessToken = type + " " + token;
+            }
+        }
+        return accessToken;
     }
 }
