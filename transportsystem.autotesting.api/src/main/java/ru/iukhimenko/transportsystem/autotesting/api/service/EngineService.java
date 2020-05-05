2e82997b045c288ecdbaeb5b7b978777336ec3dc
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-05-05 22:05:01 +0300

    Added EngineService

---
+++ transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/EngineService.java
@@ -0,0 +1,61 @@
+package ru.iukhimenko.transportsystem.autotesting.api.service;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.json.JSONException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import ru.iukhimenko.transportsystem.autotesting.api.ILogMessageHelper;
+import ru.iukhimenko.transportsystem.autotesting.api.http.Http;
+import ru.iukhimenko.transportsystem.autotesting.core.model.Engine;
+import ru.iukhimenko.transportsystem.autotesting.core.model.User;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.*;
+
+public class EngineService implements ILogMessageHelper {
+    private Logger logger = LoggerFactory.getLogger(AuthService.class);
+    private User actor;
+
+    public EngineService(User actor) {
+        this.actor = actor;
+    }
+
+    public Integer addEngine(Engine engine) {
+        Integer engineId = null;
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", new AuthService().getAccessToken(actor));
+        HttpResponse<JsonNode> response = Http.sendPostRequest(ENGINES_ADD_ENDPOINT, headers, engine);
+        if (response.isSuccess()) {
+            logger.info(getRequestExecutionResultMessage(ENGINES_ADD_ENDPOINT, response.getStatus()));
+            try {
+                engineId = response.getBody().getObject().getInt("id");
+                logger.info("Engine has been created, id = " + engineId);
+            }
+            catch (JSONException ex) {
+                logger.warn(ex.getMessage());
+            }
+        }
+        else
+            logger.info(response.getBody().getObject().toString());
+        return engineId;
+    }
+
+    public Engine getEngine(Integer id) {
+        Engine engine = null;
+        Map<String, String> headers = new HashMap();
+        headers.put("Authorization", new AuthService().getAccessToken(actor));
+        HttpResponse<JsonNode> response = Http.sendGetRequest(ENGINES_ENGINE_ENDPOINT(id), headers);
+        if (response.isSuccess()) {
+            engine = ObjectConverter.convertToObject(response.getBody().getObject(), Engine.class);
+            if (engine == null)
+                logger.warn("Failed to map engine");
+            logger.info(getRequestExecutionResultMessage(ENGINES_ENGINE_ENDPOINT(id), response.getStatus()));
+        }
+        else
+            logger.info(response.getBody().getObject().toString());
+        return engine;
+    }
+}
