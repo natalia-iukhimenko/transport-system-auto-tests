82919d6e98c9ba5300c92b560b156117e1b0fa0a
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-05-05 22:04:37 +0300

    Added Engine class

---
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/Engine.java
@@ -0,0 +1,61 @@
+package ru.iukhimenko.transportsystem.autotesting.core.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Engine {
+    private Integer id;
+    private String name;
+    private Integer volume;
+    private String fuel;
+
+    public Engine() {}
+
+    public Engine(String name, Integer volume, String fuel) {
+        this.name = name;
+        this.volume = volume;
+        this.fuel = fuel;
+    }
+
+    @JsonIgnore
+    public Integer getId() {
+        return id;
+    }
+
+    @JsonProperty("id")
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    @JsonProperty("name")
+    public String getName() {
+        return name;
+    }
+
+    @JsonProperty("name")
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @JsonProperty("volume")
+    public void setVolume(Integer volume) {
+        this.volume = volume;
+    }
+
+    @JsonProperty("volume")
+    public Integer getVolume() {
+        return volume;
+    }
+
+    @JsonProperty("fuel")
+    public void setFuel(String fuel) {
+        this.fuel = fuel;
+    }
+
+    @JsonProperty("fuel")
+    public String getFuel() {
+        return fuel;
+    }
+}
