00da096d5e9260f67dbf7593afc7f02128cd8692
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-05-17 21:01:10 +0300

    Set Jackson as default ObjectMapper

--- transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/ApiTest.java
+++ transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/ApiTest.java
@@ -1,5 +1,5 @@
 package ru.iukhimenko.transportsystem.autotesting.api;
-
+import kong.unirest.JacksonObjectMapper;
 import kong.unirest.Unirest;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,10 +9,16 @@ import static ru.iukhimenko.transportsystem.autotesting.api.Configs.*;
 public class ApiTest {
     @BeforeAll
     public static void setUp() {
-        Unirest.config().addDefaultHeader("Content-Type", "application/json");
         BASE_URI = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "baseURI");
         ADMIN_USERNAME = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminUsername");
         ADMIN_PASSWORD = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminPassword");
+        configureUnirest();
+    }
+
+    private static void configureUnirest() {
+        Unirest.config()
+                .addDefaultHeader("Content-Type", "application/json")
+                .setObjectMapper(new JacksonObjectMapper());
     }
 
     @AfterAll
