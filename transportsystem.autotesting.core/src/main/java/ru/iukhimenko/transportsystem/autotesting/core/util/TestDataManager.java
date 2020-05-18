4c2bf10d6d8dacd06c2f3d3e067a731bff5cba5f
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-05-18 20:47:59 +0300

    Added methods for generating vin and number

--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/util/TestDataManager.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/util/TestDataManager.java
@@ -1,6 +1,10 @@
 package ru.iukhimenko.transportsystem.autotesting.core.util;
 
 import com.github.javafaker.Faker;
+import com.github.javafaker.service.FakeValuesService;
+import com.github.javafaker.service.RandomService;
+
+import java.util.Locale;
 
 public class TestDataManager {
     public static String getValidPassword() {
@@ -12,5 +16,17 @@ public class TestDataManager {
     public static String getValidUsername() {
         String validUsername = "_" + new Faker().name().username();
         return validUsername;
+    }
+
+    public static String getCarNumber() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("ru-RU"), new RandomService());
+        String carNumber = fakeValuesService.regexify("[АВЕКМНОРСТУХ]\\d{3}[АВЕКМНОРСТУХ]{2}\\d{2,3}");
+        return carNumber;
+    }
+
+    public static String getVinNumber() {
+        FakeValuesService fakeValuesService = new FakeValuesService(new Locale("en-GB"), new RandomService());
+        String vinNumber = fakeValuesService.regexify("[ABCDEFGHJKLMNPRSTUVWXYZ0-9]{16}");
+        return vinNumber;
     }
 }
