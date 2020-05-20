c8186972326c600fbe6ca21117e44858531d19af
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-05-20 13:32:40 +0300

    Fixed import

--- transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/ApiTest.java
+++ transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/ApiTest.java
@@ -1,6 +1,6 @@
 package ru.iukhimenko.transportsystem.autotesting.api;
-import kong.unirest.JacksonObjectMapper;
 import kong.unirest.Unirest;
+import kong.unirest.jackson.JacksonObjectMapper;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import ru.iukhimenko.transportsystem.autotesting.core.util.FileUtils;
