1dc5cdbfe88057040aed60b165f682e37e994546
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-06-13 18:32:14 +0300

    Fixed logInButton selector

--- transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/pages/LogInPage.java
+++ transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/pages/LogInPage.java
@@ -3,12 +3,14 @@ package ru.iukhimenko.transportsystem.autotesting.ui.pages;
 import static com.codeborne.selenide.Condition.*;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import ru.iukhimenko.transportsystem.autotesting.ui.base.IPage;
+
 import static com.codeborne.selenide.Selenide.$;
 
-public class LogInPage {
+public class LogInPage implements IPage {
     private SelenideElement loginField = $(By.id("input-1"));
-    private SelenideElement passwordField = $(By.id("input-2"));;
-    private SelenideElement logInButton = $(By.xpath("//button/span[text()='Войти']"));;
+    private SelenideElement passwordField = $(By.id("input-2"));
+    private SelenideElement logInButton = $(By.id("signin"));
 
     public LogInPage setLogin(String login) {
         loginField.setValue(login);
@@ -24,6 +26,11 @@ public class LogInPage {
         logInButton.click();
     }
 
+    public RegistrationPage clickRegisterButton() {
+        $(By.id("signup")).click();
+        return new RegistrationPage();
+    }
+
     public void logInWith(String login, String password) {
         setLogin(login);
         setPassword(password);
@@ -34,18 +41,19 @@ public class LogInPage {
         return $(By.xpath("//div[@role='alert' and text()='Неверный логин или пароль!']"))
                 .shouldBe(visible)
                 .isDisplayed();
-
     }
 
+    public boolean hasLogInLegend() {
+        SelenideElement legend = $(By.xpath("//legend[text()='Вход в систему']"));
+        legend.waitUntil(visible, 10000);
+        return legend.isDisplayed();
+    }
+    
     public boolean isLoginFieldHighlightedAsInvalid() {
         return isFieldHighlightedAsInvalid(loginField);
     }
 
     public boolean isPasswordFieldHighlightedAsInvalid() {
         return isFieldHighlightedAsInvalid(passwordField);
-    }
-
-    private boolean isFieldHighlightedAsInvalid(SelenideElement field) {
-        return field.attr("aria-invalid") != null;
     }
 }
