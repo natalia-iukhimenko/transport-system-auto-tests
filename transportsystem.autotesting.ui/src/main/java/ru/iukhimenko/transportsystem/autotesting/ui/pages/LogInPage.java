483aad9776fd706e5adb9f6315bdb2b2430977c3
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-06-13 18:51:49 +0300

    Added logger and log messages

--- transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/pages/LogInPage.java
+++ transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/pages/LogInPage.java
@@ -3,22 +3,27 @@ package ru.iukhimenko.transportsystem.autotesting.ui.pages;
 import static com.codeborne.selenide.Condition.*;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.iukhimenko.transportsystem.autotesting.ui.base.IPage;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class LogInPage implements IPage {
+    private Logger logger = LoggerFactory.getLogger(LogInPage.class);
     private SelenideElement loginField = $(By.id("input-1"));
     private SelenideElement passwordField = $(By.id("input-2"));
     private SelenideElement logInButton = $(By.id("signin"));
 
     public LogInPage setLogin(String login) {
         loginField.setValue(login);
+        logger.info("Typed login '" + login + "' on authorization page");
         return this;
     }
 
     public LogInPage setPassword(String password) {
         passwordField.setValue(password);
+        logger.info("Typed password '" + password + "' on authorization page");
         return this;
     }
 
@@ -28,6 +33,7 @@ public class LogInPage implements IPage {
 
     public RegistrationPage clickRegisterButton() {
         $(By.id("signup")).click();
+        logger.info("Clicked 'Log in' button on authorization page");
         return new RegistrationPage();
     }
 
@@ -48,7 +54,7 @@ public class LogInPage implements IPage {
         legend.waitUntil(visible, 10000);
         return legend.isDisplayed();
     }
-    
+
     public boolean isLoginFieldHighlightedAsInvalid() {
         return isFieldHighlightedAsInvalid(loginField);
     }
