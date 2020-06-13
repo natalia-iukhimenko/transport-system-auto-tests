0895fd326c543929d55c1365f5e81a9918ab9368
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-06-13 18:29:44 +0300

    Added RegistrationPage class

---
+++ transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/pages/RegistrationPage.java
@@ -0,0 +1,58 @@
+package ru.iukhimenko.transportsystem.autotesting.ui.pages;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import ru.iukhimenko.transportsystem.autotesting.ui.base.IPage;
+
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+
+public class RegistrationPage implements IPage {
+    private SelenideElement loginField = $(By.id("input-1"));
+    private SelenideElement passwordField = $(By.id("input-2"));
+    private SelenideElement registerButton = $(By.id("signup"));
+
+    public RegistrationPage setLogin(String login) {
+        loginField.setValue(login);
+        return this;
+    }
+
+    public RegistrationPage setPassword(String password) {
+        passwordField.setValue(password);
+        return this;
+    }
+
+    public LogInPage clickRegisterButton() {
+        registerButton.click();
+        return new LogInPage();
+    }
+
+    public LogInPage registerAs(String login, String password) {
+        this.setLogin(login)
+                .setPassword(password)
+                .clickRegisterButton();
+        return new LogInPage();
+    }
+
+    public String getLoginFieldPromptText() {
+        return $(By.id("input-group-1")).text();
+    }
+
+    public String getPasswordFieldPromptText() {
+        return $(By.id("input-group-2")).text();
+    }
+
+    public boolean isLoginFieldHighlightedAsInvalid() {
+        return isFieldHighlightedAsInvalid(loginField);
+    }
+
+    public boolean isPasswordFieldHighlightedAsInvalid() {
+        return isFieldHighlightedAsInvalid(passwordField);
+    }
+
+    public boolean showsLoginIsBusy() {
+        return $(By.xpath("//div[text()='Логин уже занят!']"))
+                .shouldBe(visible)
+                .isDisplayed();
+    }
+}
