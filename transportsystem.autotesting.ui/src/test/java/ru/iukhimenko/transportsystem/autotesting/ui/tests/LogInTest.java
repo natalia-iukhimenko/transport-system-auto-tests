62a5a7fd90ebd34dcc28b52ad3bf1fadd25c41c5
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-06-14 20:28:04 +0300

    Added 'usernameIsDisplayedAfterSuccessfulAuthenticationTest' test

--- transportsystem.autotesting.ui/src/test/java/ru/iukhimenko/transportsystem/autotesting/ui/tests/LogInTest.java
+++ transportsystem.autotesting.ui/src/test/java/ru/iukhimenko/transportsystem/autotesting/ui/tests/LogInTest.java
@@ -2,14 +2,17 @@ package ru.iukhimenko.transportsystem.autotesting.ui.tests;
 
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import ru.iukhimenko.transportsystem.autotesting.ui.base.UiTest;
+import ru.iukhimenko.transportsystem.autotesting.ui.pages.HomePage;
 import ru.iukhimenko.transportsystem.autotesting.ui.pages.LogInPage;
 import ru.iukhimenko.transportsystem.autotesting.ui.tags.UiRegression;
+import ru.iukhimenko.transportsystem.autotesting.ui.tags.UiSmoke;
 
 import java.util.stream.Stream;
 import static com.codeborne.selenide.Selenide.open;
@@ -19,6 +22,17 @@ import static org.assertj.core.api.Assertions.assertThat;
 @Tag("ui_auth")
 @UiRegression
 public class LogInTest extends UiTest {
+    @Test
+    @Disabled
+    @UiSmoke
+    @Severity(SeverityLevel.BLOCKER)
+    public void usernameIsDisplayedAfterSuccessfulAuthenticationTest() {
+        String username = ADMIN_USERNAME, password = ADMIN_PASSWORD;
+        HomePage homePage = open(BASE_URI, LogInPage.class).logInWith(username, password);
+        String displayedUsername = homePage.getPageHeader().getDisplayedUsername();
+        assertThat(displayedUsername).isEqualTo(username);
+    }
+
     @ParameterizedTest
     @MethodSource("provideWrongUsernamePasswordPair")
     @Severity(SeverityLevel.CRITICAL)
