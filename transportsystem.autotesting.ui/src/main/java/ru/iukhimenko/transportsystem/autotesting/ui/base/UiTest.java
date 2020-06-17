ad71d37f23bb823bca7196978f9641079f174cd7
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-06-17 20:58:58 +0300

    Attach page screenshot to allure report on test failure

--- transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/base/UiTest.java
+++ transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/base/UiTest.java
@@ -1,11 +1,12 @@
 package ru.iukhimenko.transportsystem.autotesting.ui.base;
 
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
 import org.junit.jupiter.api.BeforeAll;
 import ru.iukhimenko.transportsystem.autotesting.core.util.FileUtils;
-
 import static ru.iukhimenko.transportsystem.autotesting.core.Configs.*;
 import static ru.iukhimenko.transportsystem.autotesting.core.Configs.CONFIG_FILE_PATH;
+import io.qameta.allure.selenide.AllureSelenide;
 
 public class UiTest {
     @BeforeAll
@@ -14,5 +15,7 @@ public class UiTest {
         ADMIN_USERNAME = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminUsername");
         ADMIN_PASSWORD = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminPassword");
         Configuration.browser = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "browser");
+        Configuration.screenshots = false;
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 }
