a77543d6c286ca2d3b8ed9b0028b0618e7d8746d
Natalia Yukhimenko <natalia.yukhimenko@gmail.com>
2020-07-01 19:49:24 +0300

    Created method reading json objects from file

--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/util/FileUtils.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/util/FileUtils.java
@@ -1,9 +1,15 @@
 package ru.iukhimenko.transportsystem.autotesting.core.util;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.Properties;
+import java.util.*;
 
 public class FileUtils {
     static Logger logger = LoggerFactory.getLogger(FileUtils.class);
@@ -19,5 +25,31 @@ public class FileUtils {
             e.printStackTrace();
         }
         return value;
+    }
+
+    public static <T> List<T> readJsonObjectsFromFile(String filePath, Class<T> valueType) {
+        List<T> mappedObjects = new ArrayList<>();
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            FileInputStream fileInputStream = new FileInputStream(filePath);
+            Class<T[]> arrayClass = (Class<T[]>) Class.forName("[L" + valueType.getName() + ";");
+            mappedObjects = Arrays.asList(mapper.readValue(fileInputStream, arrayClass));
+        } catch (FileNotFoundException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        } catch (JsonParseException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        } catch (JsonMappingException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            logger.warn(e.getMessage());
+            e.printStackTrace();
+        }
+        return mappedObjects;
     }
 }
