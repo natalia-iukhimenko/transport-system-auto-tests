8432d298778ae7f72c608924758ea0d4e2751ba6
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-05 19:40:59 +0300

    Removed unnecessary import

--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/Configs.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/Configs.java
@@ -2,8 +2,8 @@ package ru.iukhimenko.transportsystem.autotesting.core;
 
 public class Configs {
    public static final String CONFIG_FILE_PATH = "/config.properties";
-    public static String ADMIN_USERNAME = "admin";
-    public static String ADMIN_PASSWORD = "admin";
+    public static String ADMIN_USERNAME = "";
+    public static String ADMIN_PASSWORD = "";
     public static final String DEFAULT_URI = "http://localhost";
     public static String BASE_URI = DEFAULT_URI;
 }
