d5b1b0a9e9acdf262f898e7bc3b43d265942302c
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-21 22:35:14 +0300

    Added Lombok annotations in User class

--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/User.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/User.java
@@ -1,67 +1,17 @@
 package ru.iukhimenko.transportsystem.autotesting.core.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.*;
+import static com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
 public class User {
+    @JsonInclude(NON_NULL)
     private Integer id;
-    private String username;
-    private String password;
-
-    public User() {}
-
-    public User(String username, String password) {
-        this.username = username;
-        this.password = password;
-    }
-
-    @JsonIgnore
-    public Integer getId() {
-        return id;
-    }
-
-    @JsonProperty("id")
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    @JsonProperty("username")
-    public String getUsername() {
-        return username;
-    }
-
-    @JsonProperty("username")
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    @JsonProperty("password")
-    public String getPassword() {
-        return password;
-    }
-
-    @JsonIgnore
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    @Override
-    public boolean equals(Object object) {
-        if (object == this)
-            return true;
-        if (object == null || object.getClass() != this.getClass())
-            return false;
-        User user = (User)object;
-        return (username != null && username.equals(user.username));
-    }
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((username == null) ? 0 : username.hashCode());
-        return result;
-    }
+    private @NonNull String username;
+    private @NonNull String password;
 }
