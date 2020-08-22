7a8c6f5d533a1be87dd93889472dd1633c24abe5
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-22 21:29:23 +0300

    Added @JsonProperty annotation to Engine properties

--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/Engine.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/Engine.java
@@ -3,6 +3,8 @@ package ru.iukhimenko.transportsystem.autotesting.core.model;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import static com.fasterxml.jackson.annotation.JsonInclude.Include.*;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,8 +16,11 @@ import lombok.Setter;
 public class Engine {
     @JsonInclude(NON_NULL)
     private Integer id;
+    @JsonProperty
     private String name;
+    @JsonProperty
     private Integer volume;
+    @JsonProperty
     private String fuel;
 
     public Engine(String name, Integer volume, String fuel) {
