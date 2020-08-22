966d611438d129c25d36572b15da21a885085be2
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2020-08-22 20:34:00 +0300

    Added lombok @Builder annotation to TransportDocument class

--- transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/objectpools/TransportDocumentsPool.java
+++ transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/objectpools/TransportDocumentsPool.java
@@ -5,7 +5,6 @@ import ru.iukhimenko.transportsystem.autotesting.api.service.TransportDocumentSe
 import ru.iukhimenko.transportsystem.autotesting.core.ObjectPool;
 import ru.iukhimenko.transportsystem.autotesting.core.model.TransportDocument;
 import ru.iukhimenko.transportsystem.autotesting.core.util.TestDataManager;
-
 import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TransportDocumentsPool extends ObjectPool<TransportDocument> {
@@ -23,14 +22,14 @@ public class TransportDocumentsPool extends ObjectPool<TransportDocument> {
     @Override
     protected TransportDocument create() {
         Integer vehicleId = new VehiclesPool(1).get().getId();
-        TransportDocument documentToCreate = new TransportDocument.TransportDocumentBuilder()
-                .setDocumentType("Auto-generated document")
-                .setSeries(TestDataManager.getUniqueNumericCombination("###"))
-                .setNumber(TestDataManager.getUniqueNumericCombination("#########"))
-                .setIssuedDate("2020-02-25")
-                .setIssuedBy(new Faker().company().name())
-                .setExpireDate("2025-02-25")
-                .setTransportId(vehicleId)
+        TransportDocument documentToCreate = TransportDocument.builder()
+                .documentType("Auto-generated document")
+                .series(TestDataManager.getUniqueNumericCombination("###"))
+                .number(TestDataManager.getUniqueNumericCombination("#########"))
+                .issuedDate("2020-02-25")
+                .issuedBy(new Faker().company().name())
+                .expireDate("2025-02-25")
+                .transportId(vehicleId)
                 .build();
         TransportDocumentService service = new TransportDocumentService();
         service.addTransportDocument(documentToCreate);
--- transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/TransportDocument.java
+++ transportsystem.autotesting.core/src/main/java/ru/iukhimenko/transportsystem/autotesting/core/model/TransportDocument.java
@@ -1,133 +1,31 @@
 package ru.iukhimenko.transportsystem.autotesting.core.model;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonPOJOBuilder;
+import lombok.Builder;
 
 @JsonDeserialize(builder = TransportDocument.TransportDocumentBuilder.class)
+@Builder
 public class TransportDocument {
     private final Integer id;
+    @JsonProperty("documenttype")
     private final String documentType;
+    @JsonProperty
     private final String series;
+    @JsonProperty
     private final String number;
+    @JsonProperty("issuedby")
     private final String issuedBy;
+    @JsonProperty("issueddate")
     private final String issuedDate;
+    @JsonProperty("expiredate")
     private final String expireDate;
+    @JsonProperty("transportid")
     private final Integer transportId;
 
-    public TransportDocument(TransportDocument.TransportDocumentBuilder builder) {
-        this.id = builder.id;
-        this.documentType = builder.documentType;
-        this.series = builder.series;
-        this.number = builder.number;
-        this.issuedBy = builder.issuedBy;
-        this.issuedDate = builder.issuedDate;
-        this.expireDate = builder.expireDate;
-        this.transportId = builder.transportId;
-    }
-
-    @JsonIgnore
-    public Integer getId() {
-        return id;
-    }
-
-    @JsonProperty("documenttype")
-    public String getDocumentType() {
-        return documentType;
-    }
-
-    @JsonProperty("series")
-    public String getSeries() {
-        return series;
-    }
-
-    @JsonProperty("number")
-    public String getNumber() {
-        return number;
-    }
-
-    @JsonProperty("issuedby")
-    public String getIssuedBy() {
-        return issuedBy;
-    }
-
-    @JsonProperty("issueddate")
-    public String getIssuedDate() {
-        return issuedDate;
-    }
-
-    @JsonProperty("expiredate")
-    public String getExpireDate() {
-        return expireDate;
-    }
-
-    @JsonProperty("transportid")
-    public Integer getTransportId() {
-        return transportId;
-    }
-
     @JsonIgnoreProperties(ignoreUnknown = true)
-    public static class TransportDocumentBuilder {
-        private Integer id;
-        private String documentType;
-        private String series;
-        private String number;
-        private String issuedBy;
-        private String issuedDate;
-        private String expireDate;
-        private Integer transportId;
-
-        @JsonProperty("id")
-        public TransportDocumentBuilder setId(Integer id) {
-            this.id = id;
-            return this;
-        }
-
-        @JsonProperty("documenttype")
-        public TransportDocumentBuilder setDocumentType(String documentType) {
-            this.documentType = documentType;
-            return this;
-        }
-
-        @JsonProperty("series")
-        public TransportDocumentBuilder setSeries(String series) {
-            this.series = series;
-            return this;
-        }
-
-        @JsonProperty("number")
-        public TransportDocumentBuilder setNumber(String number) {
-            this.number = number;
-            return this;
-        }
-
-        @JsonProperty("issuedby")
-        public TransportDocumentBuilder setIssuedBy(String issuedBy) {
-            this.issuedBy = issuedBy;
-            return this;
-        }
-
-        @JsonProperty("issueddate")
-        public TransportDocumentBuilder setIssuedDate(String issuedDate) {
-            this.issuedDate = issuedDate;
-            return this;
-        }
-
-        @JsonProperty("expiredate")
-        public TransportDocumentBuilder setExpireDate(String expireDate) {
-            this.expireDate = expireDate;
-            return this;
-        }
-
-        @JsonProperty("transportid")
-        public TransportDocumentBuilder setTransportId(Integer transportId) {
-            this.transportId = transportId;
-            return this;
-        }
-
-        public TransportDocument build() {
-            return new TransportDocument(this);
-        }
-    }
+    @JsonPOJOBuilder(withPrefix = "")
+    public static class TransportDocumentBuilder {}
 }
