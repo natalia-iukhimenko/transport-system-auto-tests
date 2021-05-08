9043def85db5b5d7e9e788f1a1e939335274bc7d
Natalia Iukhimenko <natalia.yukhimenko@gmail.com>
2021-05-08 19:01:55 +0300

    Added selenoid configuration

--- transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/base/UiTest.java
+++ transportsystem.autotesting.ui/src/main/java/ru/iukhimenko/transportsystem/autotesting/ui/base/UiTest.java
@@ -2,20 +2,27 @@ package ru.iukhimenko.transportsystem.autotesting.ui.base;
 
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.logevents.SelenideLogger;
-import org.junit.jupiter.api.BeforeAll;
-import ru.iukhimenko.transportsystem.autotesting.core.util.FileUtils;
-import static ru.iukhimenko.transportsystem.autotesting.core.Configs.*;
-import static ru.iukhimenko.transportsystem.autotesting.core.Configs.CONFIG_FILE_PATH;
 import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import ru.iukhimenko.transportsystem.autotesting.core.util.FileUtils;
+
+import static ru.iukhimenko.transportsystem.autotesting.core.Configs.*;
 
 public class UiTest {
     @BeforeAll
     public static void setUp() {
+        Configuration.remote = "http://localhost:4444/wd/hub";
+        Configuration.browser = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "browser");
+        Configuration.browserSize = "1920x1080";
+        Configuration.screenshots = false;
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.browserCapabilities = capabilities;
         BASE_URI = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "baseURL");
         ADMIN_USERNAME = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminUsername");
         ADMIN_PASSWORD = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "adminPassword");
-        Configuration.browser = FileUtils.getValueFromProperties(CONFIG_FILE_PATH, "browser");
-        Configuration.screenshots = false;
         SelenideLogger.addListener("AllureSelenide", new AllureSelenide().screenshots(true).savePageSource(true));
     }
 }
