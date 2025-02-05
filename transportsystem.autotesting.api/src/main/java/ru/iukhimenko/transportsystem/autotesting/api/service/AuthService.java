4210b5d1731c3be8f54a3849129bc6ce81f6f190
natalia.iukhimenko <natalia.yukhimenko@gmail.com>
2025-02-05 21:26:58 +0100

    Minor improvements in AuthService class

--- transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/AuthService.java
+++ transportsystem.autotesting.api/src/main/java/ru/iukhimenko/transportsystem/autotesting/api/service/AuthService.java
@@ -8,6 +8,7 @@ import org.slf4j.LoggerFactory;
 import ru.iukhimenko.transportsystem.autotesting.api.http.Http;
 import ru.iukhimenko.transportsystem.autotesting.core.model.User;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -16,7 +17,7 @@ import static ru.iukhimenko.transportsystem.autotesting.api.AppEndpoints.*;
 import static ru.iukhimenko.transportsystem.autotesting.core.TransportSystemConfig.TRANSPORT_SYSTEM_CONFIG;
 
 public class AuthService extends ApiService {
-    private Logger logger = LoggerFactory.getLogger(AuthService.class);
+    private final Logger logger = LoggerFactory.getLogger(AuthService.class);
 
     public boolean registerUser(User user) {
         HttpResponse<JsonNode> response = Http.sendPostRequest(REGISTER_ENDPOINT, user);
@@ -24,34 +25,21 @@ public class AuthService extends ApiService {
     }
 
     public User authenticateUser(User user) {
-        User authenticatedUser = null;
+        User authenticatedUser = new User();
         if (user != null) {
             HttpResponse<JsonNode> response = Http.sendPostRequest(AUTH_ENDPOINT, user);
             if (response.isSuccess()) {
                 authenticatedUser = ObjectConverter.convertToObject(response.getBody().getObject(), User.class);
-                if (authenticatedUser == null)
-                    logger.warn("Failed to map user after authentication");
+            } else {
+                logger.warn("POST {} ended up with status  = {} - {}", AUTH_ENDPOINT, response.getStatus(), response.getStatusText());
             }
         } else
-            logger.info("Authentication of null user cannot be performed");
+            logger.warn("Authentication of null user cannot be performed");
         return authenticatedUser;
     }
 
-    public List<User> getUsers() {
-        List<User> users = null;
-        Map<String, String> headers = new HashMap<>();
-        headers.put("Authorization", getAccessToken(new User(TRANSPORT_SYSTEM_CONFIG.adminUsername(), TRANSPORT_SYSTEM_CONFIG.adminPassword())));
-        HttpResponse<JsonNode> response = Http.sendGetRequest(USERS_ENDPOINT, headers);
-        if (response.isSuccess()) {
-            users = ObjectConverter.convertToObjects(response.getBody().getArray(), User.class);
-            if (users == null)
-                logger.warn("Failed to map users");
-        }
-        return users;
-    }
-
     public String getAccessToken(User user) {
-        String accessToken = null;
+        String accessToken = "";
         HttpResponse<JsonNode> response = Http.sendPostRequest(AUTH_ENDPOINT, user);
         if (response.isSuccess()) {
             String type = response.getBody().getObject().getString("type");
@@ -59,7 +47,22 @@ public class AuthService extends ApiService {
             if (StringUtils.isNotEmpty(type) && StringUtils.isNotEmpty(token)) {
                 accessToken = type + " " + token;
             }
+        } else {
+            logger.warn("POST {} ended up with status  = {} - {}", AUTH_ENDPOINT, response.getStatus(), response.getStatusText());
         }
         return accessToken;
+    }
+
+    public List<User> getUsers() {
+        List<User> users = new ArrayList<>();
+        Map<String, String> headers = new HashMap<>();
+        headers.put("Authorization", getAccessToken(new User(TRANSPORT_SYSTEM_CONFIG.adminUsername(), TRANSPORT_SYSTEM_CONFIG.adminPassword())));
+        HttpResponse<JsonNode> response = Http.sendGetRequest(USERS_ENDPOINT, headers);
+        if (response.isSuccess()) {
+            users = ObjectConverter.convertToObjects(response.getBody().getArray(), User.class);
+        } else {
+            logger.warn("GET {} ended up with status  = {} - {}", USERS_ENDPOINT, response.getStatus(), response.getStatusText());
+        }
+        return users;
     }
 }
